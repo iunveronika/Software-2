e7afcaec8025960854f5093655c22fb9386390a2
iunveronika <143784596+iunveronika@users.noreply.github.com>
2023-12-09 02:11:49 +0200

    Create calculateValues.js

---
+++ Exercises/JavaScript/Module 1/1.3/calculateValues.js
@@ -0,0 +1,16 @@
+function calculateValues() {
+    var num1 = parseInt(prompt("Enter the first integer:"));
+    var num2 = parseInt(prompt("Enter the second integer:"));
+    var num3 = parseInt(prompt("Enter the third integer:"));
+
+    var sum = num1 + num2 + num3;
+    var product = num1 * num2 * num3;
+    var average = sum / 3;
+
+    document.getElementById("results").innerHTML = 
+        "Sum: " + sum + "<br>" +
+        "Product: " + product + "<br>" +
+        "Average: " + average.toFixed(2);
+}
+
+window.onload = calculateValues;
